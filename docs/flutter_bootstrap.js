4c7b0a1ab015b634f397a6d98b20e9fd189aa632
HFFSolutions <hericfreitas.empresa@gmail.com>
2025-02-19 11:08:59 -0300

    teste

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "683689543"
+    serviceWorkerVersion: "1914663438"
   }
 });
--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -43,7 +43,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "c245fdfb1d696eef567692974e4ab983
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "fb04b663b6d73d80bb5193e6a7df177d",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "c69600ac8f4512ff492c8d161b633fa3",
+"flutter_bootstrap.js": "beeffcaa70d2d607f181a0ca66e5dd84",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
